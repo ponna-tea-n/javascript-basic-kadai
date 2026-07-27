5043df897d12e9155c817448863fc4a226cb4a20
ponna-tea-n <302549788+ponna-tea-n@users.noreply.github.com>
2026-07-28 00:58:04 +0900

    kadao_005をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+//console.log('a');
+
+function bmiCalculator(weight, height) {
+    return weight / (height * height);
+}
+
+const bmi = bmiCalculator(68,1.7);
+console.log(bmi);
+
+/*
+console.log(bmiCalculator(100,1.7));
+*/
