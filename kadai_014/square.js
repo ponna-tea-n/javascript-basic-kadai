c4268888bfc1adc555ba79eb959326938f6ca682
ponna-tea-n <302549788+ponna-tea-n@users.noreply.github.com>
2026-07-28 15:22:54 +0900

    kadai_014を提出

---
+++ kadai_014/square.js
@@ -0,0 +1,50 @@
+
+
+function square(num) {
+  return num * num;
+}
+
+const result = square(10);
+console.log(result);
+
+/*
+const square = (num)=>{
+  return num * num;
+}
+console.log(square(10));
+*/
+
+
+
+/*
+★質問内容★
+●アロー１
+const square = (num) => {
+  return num * num;
+};
+
+const result = square(10);
+console.log(result);
+
+
+●アロー2
+const square = (num) => num * num;
+
+console.log(square(10));
+
+
+●function１
+function square(num) {
+  return num * num;
+}
+
+console.log(square(10));
+
+
+
+
+
+
+
+
+*/
